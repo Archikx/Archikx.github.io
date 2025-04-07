164ff1741e34db87d046b2791faf3374b5fce2fc
Waffles <159680939+Archikx@users.noreply.github.com>
2025-04-07 20:08:41 +0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,34 +1,86 @@
-var obj
+var objpeople = [
+    { username: "Waffles", password: "Исследователь" },
+    { username: "Gesugao", password: "019" },
+    { username: "Danya", password: "910" },
+];
+
 var codes = [
-    {
-        data_get: "data1",
-    },
-    {
-        data_get: "data2",
-    },
-    {
-        data_get: "data3",
-    },
+    "data1",
+    "data2",
+    "data3"
 ];
-var answers = [
-    4,
-    5,
-    6
-];
+
+
+
 function getinfo() {
-    var data_get = document.getElementById("data_get").value
+    var username = document.getElementById("username").value
+    var password = document.getElementById("password").value
+
+    for(i = 0; i < objpeople.length; i++) {
+        if (username == objpeople[i].username && password == objpeople[i].password) {
+            console.log(username + " is logged in!")
+            return
+        }
+        else {
+            console.log("incorrect username or password")
+        }
+    }
 }
+
+
+function au_getinfo() {
+    var au_username = document.getElementById("au_username").value
+    var au_password = document.getElementById("au_password").value
+    var newuser = {username: au_username, password: au_password}
+
+    for (i = 0;i < objpeople.length; i++) {
+        if (au_username == objpeople[i].username) {
+            alert("Name is already used!")
+            return
+        }else if (au_password == objpeople[i].password) {
+            alert("Password is already used!")
+            return
+        }
+    }
+
+    objpeople.push(newuser)
+    console.log(objpeople)
+}
+
+
+function show() {
+    var data_get = document.getElementById("data_get").value
+    if (data_get == codes[0]) {
+        var img = document.createElement("img");
+        img.src = "img/message_minecraft.jpg";
+        var src = document.getElementById("header");
+        src.appendChild(img);
+    }else if (data_get == codes[1]) {
+        var img = document.createElement("img");
+        img.src = "img/message_minecraft.jpg";
+        var src = document.getElementById("header");
+        src.appendChild(img);
+    }else if (data_get == codes[2]) {
+        var img = document.createElement("img");
+        img.src = "img/message_minecraft.jpg";
+        var src = document.getElementById("header");
+        src.appendChild(img);
+    }else {
+        alert("Wrong code!")
+    }
+    
+}
+
+/*
 window.onload = function() {
 
     var fileInput = document.getElementById('fileInput');
     var fileDisplayArea = document.getElementById('fileDisplayArea');
-
-
     fileInput.addEventListener('change', function(e) {
         var file = fileInput.files[0];
         var imageType = /image.*/;
 
-        if (file.type.match(imageType)) {
+        /*if (file.type.match(imageType)) {
             var reader = new FileReader();
 
             reader.onload = function(e) {
@@ -47,6 +99,9 @@ window.onload = function() {
     });
 
 }
+
+
+
 
 /*var storeditem = localStorage.getItem('storeditem')
 function save() {
