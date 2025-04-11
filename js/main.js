18a7550c72700c499e810561909d6ea896b67227
Waffles <159680939+Archikx@users.noreply.github.com>
2025-04-11 20:46:31 +0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,32 +1,138 @@
 var objpeople = [
     { username: "Waffles", password: "Исследователь" },
-    { username: "Gesugao", password: "019" },
-    { username: "Danya", password: "910" },
+    { username: "Аблай", password: "" },
+    { username: "Даня", password: "" },
+    { username: "Диас", password: "" },
+    { username: "Тамур", password: "" },
+    { username: "Тимур", password: "" },
+    { username: "Паша", password: "" },
+    { username: "Алмаз", password: "" },
+    { username: "Абзал", password: "" },
+    { username: "Тигран", password: "" },
 ];
 
-var codes = "data1"
+var codes = "Свобода, Ты — светлый путь, Тебя искали, в сердцахъ наших. Ты в каждомъ шаге, ты в каждомъ дыханье. Кто жъ тебя возьмётъ, тотъ правдою силен, Тотъ и землю родную отъ страха освободит. В сердцы поколения живи навсегда!"
 
 
 
 
 function getinfo() {
     var username = document.getElementById("username").value
-    var password = document.getElementById("password").value
 
     for(i = 0; i < objpeople.length; i++) {
-        if (username == objpeople[i].username && password == objpeople[i].password) {
+        if (username == objpeople[0].username) {
             console.log(username + " is logged in!")
-            window.location.href = 'index.html';
+            localStorage.setItem('name', username);
+            window.location.href = 'html/index.html';
             return
+        }else if (username == objpeople[1].username){
+            console.log(username + " is logged in!")
+            localStorage.setItem('name1', username);
+            window.location.href = 'html/login1.html';
+        }
+        else if (username == objpeople[2].username){
+            console.log(username + " is logged in!")
+            localStorage.setItem('name2', username);
+            window.location.href = 'html/login2.html';
+        }
+        else if (username == objpeople[3].username){
+            console.log(username + " is logged in!")
+            localStorage.setItem('name3', username);
+            window.location.href = 'html/login3.html';
+        }
+        else if (username == objpeople[4].username){
+            console.log(username + " is logged in!")
+            localStorage.setItem('name4', username);
+            window.location.href = 'html/login4.html';
+        }
+        else if (username == objpeople[5].username){
+            console.log(username + " is logged in!")
+            localStorage.setItem('name5', username);
+            window.location.href = 'html/login5.html';
+        }
+        else if (username == objpeople[6].username){
+            console.log(username + " is logged in!")
+            localStorage.setItem('name6', username);
+            window.location.href = 'html/login6.html';
+        }
+        else if (username == objpeople[7].username){
+            console.log(username + " is logged in!")
+            localStorage.setItem('name7', username);
+            window.location.href = 'html/login7.html';
+        }
+        else if (username == objpeople[8].username){
+            console.log(username + " is logged in!")
+            localStorage.setItem('name8', username);
+            window.location.href = 'html/login8.html';
+        }
+        else if (username == objpeople[9].username){
+            console.log(username + " is logged in!")
+            localStorage.setItem('name9', username);
+            window.location.href = 'html/login9.html';
         }
         else {
+            alert("Нет Такого имени, первая буква должна быть заглавной");
             console.log("incorrect username or password")
+            return
         }
     }
 }
+function get_info1 (){
+    var password = document.getElementById('password').value;
+    localStorage.setItem('work1', password);
+    window.location.href = 'index.html';
+    return
+}
+function get_info2 (){
+    var password = document.getElementById('password').value;
+    localStorage.setItem('work2', password);
+    window.location.href = 'index.html';
+    return
+}
+function get_info3 (){
+    var password = document.getElementById('password').value;
+    localStorage.setItem('work3', password);
+    window.location.href = 'index.html';
+    return
+}
+function get_info4 (){
+    var password = document.getElementById('password').value;
+    localStorage.setItem('work4', password);
+    window.location.href = 'index.html';
+    return
+}
+function get_info5 (){
+    var password = document.getElementById('password').value;
+    localStorage.setItem('work5', password);
+    window.location.href = 'index.html';
+    return
+}
+function get_info6 (){
+    var password = document.getElementById('password').value;
+    localStorage.setItem('work6', password);
+    window.location.href = 'index.html';
+    return
+}
+function get_info7 (){
+    var password = document.getElementById('password').value;
+    localStorage.setItem('work7', password);
+    window.location.href = 'index.html';
+    return
+}
+function get_info8 (){
+    var password = document.getElementById('password').value;
+    localStorage.setItem('work8', password);
+    window.location.href = 'index.html';
+    return
+}
+function get_info9 (){
+    var password = document.getElementById('password').value;
+    localStorage.setItem('work9', password);
+    window.location.href = 'index.html';
+    return
+}
 
-
-function au_getinfo() {
+function get_info() {
     var au_username = document.getElementById("au_username").value
     var au_password = document.getElementById("au_password").value
     var newuser = {username: au_username, password: au_password}
